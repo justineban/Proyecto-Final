76742dd00361ebda87c209cc77f14530464af870
justineban <justineb@uninorte.edu.co>
2025-11-22 19:53:13 -0500

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto-Final-1/',
+  base: '/',
   server: {
     port: 5173,
     open: true
